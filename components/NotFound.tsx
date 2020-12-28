205cd13a5010c694072d16c46b3af2856fcb1d2a
Ben Southgate <bsouthga@gmail.com>
2020-12-28 15:40:03 -0800

    add 404

---
+++ components/NotFound.tsx
@@ -0,0 +1,5 @@
+import styles from "components/NotFound.module.css";
+
+export default function NotFound() {
+  return <div className={styles.root}>whoops, this doesn't exist...</div>;
+}
---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import Layout from "components/Layout";
+import NotFound from "components/NotFound";
+
+export default function ForOhFour() {
+  return (
+    <Layout>
+      <NotFound />
+    </Layout>
+  );
+}
