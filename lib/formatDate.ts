4cda363a90301f771bac40f5ff4eee86c9fe5627
Ben Southgate <bsouthga@gmail.com>
2020-12-30 19:29:57 -0800

    update date format

--- lib/formatDate.ts
+++ lib/formatDate.ts
@@ -1,4 +1,6 @@
+const OPTIONS = { year: "numeric", month: "long", day: "numeric" };
+
 export default function formatDate(datestr: string) {
   const date = new Date(datestr);
-  return date.toLocaleDateString();
+  return date.toLocaleDateString(undefined, OPTIONS);
 }
