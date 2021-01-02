8b276ac13f3369357d2198c66dd2cf43496847f6
Ben Southgate <bsouthga@gmail.com>
2021-01-01 19:49:11 -0800

    remove props

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,9 +1,5 @@
 import styles from "components/Loading.module.css";
 
-export type Props = {
-  pastDelay: boolean;
-};
-
 export default function Loading() {
   return (
     <div className={styles.container}>
